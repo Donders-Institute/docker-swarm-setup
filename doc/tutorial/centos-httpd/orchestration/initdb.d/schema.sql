dc3d88e81f1d90cd88341295ba818d20c994d3a7
Hong Lee <h.lee@donders.ru.nl>
2018-08-17 14:56:12 +0200

    Added unique key combining name and email

--- doc/tutorial/centos-httpd/orchestration/initdb.d/schema.sql
+++ doc/tutorial/centos-httpd/orchestration/initdb.d/schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE users (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(255),
     email varchar(255),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE KEY (name, email)
 );
