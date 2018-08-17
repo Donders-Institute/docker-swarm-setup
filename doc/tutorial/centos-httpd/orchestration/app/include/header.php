8dfb8505549dd5d05a052d3e13766fe1fad940a2
Hong Lee <h.lee@donders.ru.nl>
2018-08-17 14:55:45 +0200

    Fixed style for header/footer/navbar

--- doc/tutorial/centos-httpd/orchestration/app/include/header.php
+++ doc/tutorial/centos-httpd/orchestration/app/include/header.php
@@ -22,3 +22,16 @@
 		});
 	});
 </script>
+<style>
+main.container {
+    padding: 60px 15px 0;
+}
+.footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    background-color: #f5f5f5;
+}
+</style>
--- doc/tutorial/centos-httpd/orchestration/app/include/navbar.php
+++ doc/tutorial/centos-httpd/orchestration/app/include/navbar.php
@@ -1,11 +1,9 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin:24px 0;">
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="index.php">Register</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="report.php">Report</a>
-    </li>
-  </ul>
-</nav>
+<header>
+	<nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+  	<!-- Links -->
+  	<ul class="navbar-nav">
+    		<li class="nav-item"><a class="nav-link" href="index.php">Register</a></li>
+    		<li class="nav-item"><a class="nav-link" href="report.php">Report</a></li>
+	</ul>
+	</nav>
+</header>
