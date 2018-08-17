1e6be081ad2dd8b500bbd95424f2703f137947da
Hurng-Chun Lee <hurngchunlee@users.noreply.github.com>
2018-08-17 14:58:36 +0200

    Update header.php

--- doc/tutorial/centos-httpd/orchestration/app/include/header.php
+++ doc/tutorial/centos-httpd/orchestration/app/include/header.php
@@ -7,6 +7,7 @@
 <!-- bootstrap javascript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<!-- application specific javascripts -->
 <script type="text/javascript">
 	$(function() { //equal to $(document).ready() {
 		$('#registrationForm').submit(function() {
@@ -22,6 +23,7 @@
 		});
 	});
 </script>
+<!-- application specific css -->
 <style>
 main.container {
     padding: 60px 15px 0;
